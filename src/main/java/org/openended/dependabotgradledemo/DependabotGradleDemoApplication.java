03e92989bbc0b0b3307aa623027fe1b884d86304
Jens Fischer <jens.fischer@otto.de>
2019-06-28 14:57:06 +0200

    initial import

---
+++ src/main/java/org/openended/dependabotgradledemo/DependabotGradleDemoApplication.java
@@ -0,0 +1,13 @@
+package org.openended.dependabotgradledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependabotGradleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependabotGradleDemoApplication.class, args);
+	}
+
+}
